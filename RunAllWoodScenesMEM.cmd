fc73bfcec81c515f2b35cd8f34cfdeffc92f1f23
Tomas Karella <tomas.karella@gmail.com>
2018-05-14 19:32:44 +0200

    commited

---
+++ RunAllWoodScenesMEM.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+echo "All Scenes of wood scene will be started"
+echo "WoodCutter Walk started"
+start /wait SwarmSimFramework.exe -de DEWoodExperiments/WoodCuttorWalkMem.expe
+echo "WoodCutter Walk finnished"
+echo "WoodCutter cut started"
+start /wait SwarmSimFramework.exe -de DEWoodExperiments/WoodCuttorCutMem.expe "WoodCuttorWalkMemBrain0.json"
+echo "WoodWorker Walk started" 
+start /wait SwarmSimFramework.exe -de DEWoodExperiments/WoodWorkerWalkMem.expe
+echo "WoodWorker Walk finnished"
+echo "WoodWorker PickUp started"
+start /wait SwarmSimFramework.exe -de DEWoodExperiments/WoodWorkerPickUpMem.expe "WoodWorkerWalkMemBrain0.json"
+echo "WoodWorker PickUp finnished"
+echo "WoodWorker Stock started"
+start /wait SwarmSimFramework.exe -de DEWoodExperiments/WoodWorkerStockItemsMem.expe "WoodWorkerPickUpMemBrain0.json" 
+start /wait SwarmSimFramework.exe -de DEWoodExperiments/WoodCooperatitiveFinalMem.expe "WoodCuttorCutMemBrain0.json" "WoodWorkerWoodWorkerStoreBrain0.json"
